636616f4ccea87aea5c0f071a409698587336d0a
soumachatterjee <soumachatterjee@SOUMALYA-DEV>
2020-09-18 17:57:57 +0530

    Add  Promotion engine Logic

--- PromotionEngineCodingTest/Program.cs
+++ PromotionEngineCodingTest/Program.cs
@@ -6,7 +6,23 @@ namespace PromotionEngineCodingTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter number of Products type 'A':");
+            var AtypeProducts = Console.ReadLine();
+            Console.WriteLine("Please enter number of Products type 'B':");
+            var BtypeProducts = Console.ReadLine();
+            Console.WriteLine("Please enter number of Products type 'C':");
+            var CtypeProducts = Console.ReadLine();
+            Console.WriteLine("Please enter number of Products type 'D':");
+            var DtypeProducts = Console.ReadLine();
+            Console.WriteLine(GetPromotionItemSets(10, 3));
+            Console.ReadKey();
+        }
+        static Tuple<int, int> GetPromotionItemSets(int nuberofProducts, int offerSetValue)
+        {
+            var mean = nuberofProducts + offerSetValue / 2;
+            var nonPromotionItemCount = nuberofProducts - (mean - (mean % offerSetValue));
+            var offerSet = (mean % offerSetValue);
+            return new Tuple<int, int>(offerSet, nonPromotionItemCount);
         }
     }
 }
